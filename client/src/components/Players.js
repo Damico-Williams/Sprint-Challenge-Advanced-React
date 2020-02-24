22f5e24246b1f23f9d3a55e2fd9bb4db137f3078
damico williams <damicowilliams@gmail.com>
2020-02-24 15:21:21 -0800

    changed table color

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table } from 'semantic-ui-react'
 import useDarkMode from '../hooks/useDarkMode.js';
 import NavBar from './NavBar'
 
 const Players = props => {
     const [darkMode, setDarkMode] = useDarkMode('darkMode', false);
+    const [color, setColor] = useState('sky blue');
     const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
+    setColor(color === 'sky blue' ? 'black' : 'sky blue')
   };
 
     return(
         <div>
             <NavBar toggleMode={toggleMode} darkMode={darkMode} />
-            <Table celled striped>
+            <Table celled striped color={color} inverted>
            <Table.Body>
                   {props.player.map(players => {
                       return(
